2ce8d0332345a0a7deef41fde60abf33b5bbb5f6
sos1497 <moralesmateo277@gmail.com>
2025-10-03 23:02:40 -0500

    Correccion error program.cs

--- FrontendBlazorApi/obj/Debug/net8.0/FrontendBlazorApi.AssemblyInfo.cs
+++ FrontendBlazorApi/obj/Debug/net8.0/FrontendBlazorApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontendBlazorApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63dc73adab13b79623e02b4a859f7c37db277064")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65bdc9857dcd2bbaae9be73caa9a53f64107ab1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontendBlazorApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontendBlazorApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
