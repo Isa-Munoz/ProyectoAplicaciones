9199c1abc8d776ef391d42ce024651fc27492f61
Isa-Munoz <142456520+Isa-Munoz@users.noreply.github.com>
2025-10-30 01:20:52 -0500

    Se agrega modelos Archivo, Archivo-Entregable, Responsable, Responsable-Entregable con sus razor, las tablas de union presentan problemas al elminiar

--- FrontendBlazorApi/obj/Debug/net8.0/FrontendBlazorApi.AssemblyInfo.cs
+++ FrontendBlazorApi/obj/Debug/net8.0/FrontendBlazorApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontendBlazorApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65bdc9857dcd2bbaae9be73caa9a53f64107ab1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ce8d0332345a0a7deef41fde60abf33b5bbb5f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontendBlazorApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontendBlazorApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
